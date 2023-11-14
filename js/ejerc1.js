b77984c2923906bdbb75f0dbbdbb442047d0b34b
mariana <marianass712@gmail.com>
2023-11-14 19:16:24 -0300

    se realizo el ejericio1

---
+++ js/ejerc1.js
@@ -0,0 +1,31 @@
+/* Crea un objeto llamado auto que tenga algunas características como el color, marca, modelo y si está encendido o apagado. Crea los métodos necesarios para permitir encender y apagar el auto.
+Output:
+objeto.encender(); auto encendido
+objeto.apagar() El auto se apagó*/
+
+const auto = {
+  color: "negro",
+  marca: "Toyota",
+  modelo: "Corolla",
+  encendido: false,
+
+  encenderAuto: function () {
+    if (!this.encendido) {
+      // Verifica si el auto está apagado
+      this.encendido = true;
+      document.write("Auto encendido");
+    } else {
+      document.write("El auto ya está encendido");
+    }
+  },
+  apagarAuto: () => {
+    if (this.encendido) {
+      this.encendido = false;
+      document.write("El auto se apagó");
+    } else {
+      document.write("El auto ya está apagado");
+    }
+  },
+};
+//auto.encenderAuto();
+//auto.apagarAuto();
