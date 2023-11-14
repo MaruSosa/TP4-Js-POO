c52e558fb7b6a0699613361703b7488c4027d40b
mariana <marianass712@gmail.com>
2023-11-14 20:21:17 -0300

    se corrigió ejercicio2 y se agrego el metodo informar

--- js/ejerc2.js
+++ js/ejerc2.js
@@ -8,29 +8,32 @@ Un método informar() que retorne la información del estado de la cuenta.
 Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a mostrar la descripción del estado de la cuenta.
 Rectángulos*/
 
-const cuenta ={
-    titular: 'Alex',
-    saldo: 0,
+const cuenta = {
+  titular: "Alex",
+  saldo: 0,
 
-    ingresar: function(cantidad){
-        if (cantidad > 0) {
-            this.saldo += cantidad;
-            document.write(`Se han ingresado ${cantidad} a la cuenta`);
-          } else {
-            document.write('La cantidad ingresada debe ser mayor que 0');
-          }
-    },
-    extraer: function(cantidad){
-        if(cantidad>0 && cuenta.saldo>=cantidad){
-            this.saldo -= cantidad;
-            document.write(`Se extrajo ${cantidad} de la cuenta`);
-        }else{
-            document.write('Error al realizar la extracción');
-        }
+  ingresar: function (cantidad) {
+    if (cantidad > 0) {
+      cuenta.saldo += cantidad;
+      document.write(`Se han ingresado ${cantidad} a la cuenta`);
+    } else {
+      document.write("La cantidad ingresada debe ser mayor que 0");
     }
+  },
+  extraer: function (cantidad) {
+    if (cantidad > 0 && cuenta.saldo >= cantidad) {
+      cuenta.saldo -= cantidad;
+      document.write(`Se extrajo ${cantidad} de la cuenta`);
+    } else {
+      document.write("Error al realizar la extracción");
+    }
+  },
+  informar: function () {
+    return `Titular: ${cuenta.titular} <br> Saldo: ${cuenta.saldo}`;
+  },
 };
 document.write(`<p>cuenta: ${cuenta.titular}</p>`);
 cuenta.ingresar(2500);
-document.write(`<p>saldo: ${cuenta.saldo}</p>`);
+document.write(`<p>Informe de estado de cuenta: <br> ${cuenta.informar()}</p>`);
 cuenta.extraer(1500);
-document.write(`<p>saldo: ${cuenta.saldo}</p>`);
+document.write(`<p>Informe de estado de cuenta:<br>${cuenta.informar()}</p>`);
